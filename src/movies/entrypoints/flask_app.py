2ae80afb2e4566c9bab400cde33bd475976b0e8f
uliseslugo <uliseslf99@hotmail.com>
2022-06-12 20:19:50 -0500

    Add post endpoint for registering

--- src/movies/entrypoints/flask_app.py
+++ src/movies/entrypoints/flask_app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+import json
+from flask import Flask, request, json
 from movies import models
 
 app = Flask(__name__)
@@ -8,3 +9,17 @@ models.start_mappers()
 @app.route("/hello", methods=["GET"])
 def hello_world():
     return "Hello World!", 200
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    args = request.json
+
+    username = args.get('username')
+    email = args.get('email')
+    preferences = args.get('preferences')
+
+    if(username is None or email is None or preferences is None):
+        return 'Username, email and preferences are required for registering', 400
+
+    return "This is register :)", 200
